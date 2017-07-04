4bd09f6bf5b0b3d46b57f2a3e2ef9c36a0f0c36b
Sher <sherjunior6505@gmail.com>
2017-07-03 22:40:16 -0400

    added setup file

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# Source this.
+# Setup virtualenv
+
+sudo apt-get install -y \
+    python-pip \
+    build-essential \
+    git \
+    python \
+    python-dev
+
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+
+if [ ! -d venv ]
+then
+  virtualenv venv
+fi
+
+. venv/bin/activate
+
+apt-get install python-tk
+
+
+#pip install numpy
+# To deactivate the venv, use
+#
+# $ deactivate
+#
+# as a command on the command line.
+# To set up the venv again, then type
+#
+# $ source setup.sh
